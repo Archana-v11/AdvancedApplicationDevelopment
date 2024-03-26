a85561967e75ef0ff4f1044bc9e5071270e847c1
Archana-v11 <110531490+Archana-v11@users.noreply.github.com>
2024-03-26 16:24:05 +0530

    Add files via upload

---
+++ Day 06/src/main/java/com/birthday/aad/Entity/Themes.java
@@ -0,0 +1,30 @@
+package com.birthday.aad.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class Themes{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String packageName;
+    private String themeName;
+    private String price;
+    private String image;
+
+
+}
